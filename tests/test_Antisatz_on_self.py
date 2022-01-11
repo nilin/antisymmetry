830b04287ed5856eb1a29fbc4dc937d0c67cf551
Nilin <nilin@mit.edu>
2022-01-11 10:26:00 -0800

    added test to train ansatz on itself

---
+++ tests/test_Antisatz_on_self.py
@@ -0,0 +1,48 @@
+import antisymmetry.learning as learning
+import antisymmetry.mcmc as mcmc
+import antisymmetry.plotdata as plotdata
+import antisymmetry.train as train
+import numpy as np
+import pickle
+import time
+import matplotlib.pyplot as plt
+import jax
+import jax.numpy as jnp
+import sys
+import os
+
+
+
+
+
+
+
+
+
+def initialize_learn_self(ID,randkey,args):
+
+	paramsfolder='params/a'
+	Ansatztype='a'
+
+	params={}
+	for line in open(paramsfolder+'/default'):
+		key,val=line.split()
+		params[key]=train.cast_type(val,key)
+
+
+	randkey1,randkey2=jax.random.split(randkey)
+	X_distribution=lambda key,samples:jax.random.normal(key,shape=(samples,params['n'],params['d']))
+
+			
+	truth=learning.Antisatz(params,randkey1)
+	truth.normalize(X_distribution)
+
+	ansatz=learning.Antisatz(params,randkey2)
+	#ansatz.normalize(X_distribution)
+
+
+	train.print_params(params,paramsfolder,train.descriptions,Ansatztype)
+	return Ansatztype,truth,ansatz,params,X_distribution
+
+
+train.run(initialize_learn_self)
