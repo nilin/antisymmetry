6f768e0482d83901c9a4351bc32127718c2f5db7
Nilin <nilin@mit.edu>
2022-03-17 04:03:11 -0700

    cleanup

--- cancellations/plot_data_and_proxy.py
+++ cancellations/plot_data_and_proxy.py
@@ -10,178 +10,105 @@ import pickle
 import time
 import bookkeep as bk
 import copy
+import sys
 import jax
 import jax.numpy as jnp
 import optax
 import util
-import spherical
 import cancellation as canc
 import antisymmetry.mcmc as mcmc
 
-
+from proxies import *
 
 
 key=jax.random.PRNGKey(0)
 key,*keys=jax.random.split(key,1000)
 
-subfolder='abbrev/'
 
 activationnames=['osc','HS','ReLU','exp','tanh','DReLU']
 proxynames=['Z','OP','polyOP','polyZ','OCP','polyOCP','polyOCP_proxy','extendedpolyOP']
-defaultstyles={'Z':'g--','OP':'k--','polyOP':'k','polyZ':'g','OCP':'r--','polyOCP':'r','polyOCP_proxy':'y:','extendedpolyOP':'m'}
+defaultstyles={'Z':'g--','OP':'k--','polyOP':'k:','polyZ':'g:','OCP':'r--','polyOCP':'r:','polyOCP_proxy':'y:','extendedpolyOP':'m'}
 
 
 
-####################################################################################################
-def Znorm(key,activation,W,X):
-	z=jax.random.normal(key,shape=(10000,))
-	return util.L2norm(activation(z))
 
-def OPnorm(key,activation,W,X):
-	n,d=W.shape[-2:]
-	x=util.sample_mu(n*d,10000,key)
-	return util.L2norm(activation(x))
-
-def polyOPnorm(key,activation,W,X):
-	n,d=W.shape[-2:]
-	x=util.sample_mu(n*d,10000,key)
-	a,dist=util.polyfit(x,activation(x),n-2)
-	#p=util.poly_as_function(a)
-	#r=lambda x:activation(x)-p(x)
-	return dist
-
-def polyZnorm(key,activation,W,X):
-	n=W.shape[-2]
-	z=jax.random.normal(key,shape=(10000,))
-	a,dist=util.polyfit(z,activation(z),n-2)
-	#p=util.poly_as_function(a)
-	#r=lambda x:activation(x)-p(x)
-	return dist
-
-def OCPnorm(key,activation,W,X):
-	r_squared=jnp.sum(jnp.square(W),axis=(-2,-1))
-	eps_squared=2*jnp.square(util.mindist(W))
-	return jnp.sqrt(jnp.average(util.variations(key,activation,r_squared,eps_squared)))
-
-def polyOCPnorm(key,activation,W,X):
-	n=W.shape[-2]
-	r_squared=jnp.sum(jnp.square(W),axis=(-2,-1))
-	eps_squared=2*jnp.square(util.mindist(W))
-	key1,key2=jax.random.split(key)
-	a,dist=util.poly_fit_variations(key1,activation,n-2,r_squared,eps_squared)
-	return util.L2norm(dist)
-		
-def polyOCP_proxynorm(key,activation,W,X):
-	n,d=W.shape[-2:]
-	key1,key2=jax.random.split(key)
-	r_squared=jnp.sum(jnp.square(W),axis=(-2,-1))
-	eps_squared=2*jnp.square(util.mindist(W))
-
-	x=util.sample_mu(n*d,10000,key1)
-	a,dist=util.polyfit(x,activation(x),n-2)
-	p=util.poly_as_function(a)
-	r=lambda x:activation(x)-p(x)
-
-	return jnp.sqrt(jnp.average(util.variations(key2,r,r_squared,eps_squared)))
-
-def extendedpolyOPnorm(key,activation,W,X):
-	n,d=W.shape[-2:]
-	x=util.sample_mu(n*d,10000,key)
-	functionbasis=util.prepfunctions([util.monomials(n-2)],[lambda x:jnp.exp(-2*x),lambda x:jnp.exp(-x),jnp.exp,lambda x:jnp.exp(2*x)])
-	a,dist=util.functionfit(x,activation(x),functionbasis)
-	return dist
-
-####################################################################################################
-
-def plotproxies(activation,proxychoices,nmax,styles='default'):
-	WXname='trivial d=3'
-	W_,X_,instances,samples,n_range,d=bk.getdata(WXname)
-
-	if styles=='default':
-		styles=[defaultstyles[c] for c in proxychoices]
-
-	n_=range(2,nmax+1)
-
-	i=0
+def evalproxies(activation,proxychoices,n_,datafolder):
+	Ws,Xs=[bk.getdata(datafolder+'/WX')[k] for k in ['Ws','Xs']]
+	norms_table={}
 	for proxyname in proxychoices:	
 		compnorm=globals()[proxyname+'norm']
-		norms=[compnorm(keys[n],activation,W_[n],X_[n]) for n in n_]
-		plt.plot(n_,norms,styles[i],zorder=50-i)	
-		i=i+1
+		norms_table[proxyname]=[compnorm(keys[n],activation,Ws[n],Xs[n]) for n in n_]
+	return norms_table	
 
-def plotdata(filenames,colors,alpha=.2,subfolder=subfolder):
-	i=0
-	for filename in filenames:
-		range_,vals=bk.getplotdata(subfolder+filename)
-		plt.scatter(range_,vals,color=colors[i],zorder=100)
-		plt.plot(range_,vals,color=colors[i],alpha=alpha)
-		i=i+1
 
-def data_only(files,colors,subfolder):
-	activations=[util.activations[f] for f in files]
+"""
+color by data, style by estimate
+"""
+def multiple_activations(ac_name_color,proxy_name_style,WXfolder,datafolder):
+
 	plt.figure()
 	plt.yscale('log')
-	plotdata(files,colors,alpha=1,subfolder=subfolder)
-	plt.plot(range(1,10),1/jnp.sqrt(jnp.array([math.factorial(n) for n in range(1,10)])),'r:')
-	savename=' '.join(files)
-	plt.savefig('plots/'+subfolder+savename+'.pdf')
-
-def data_and_proxy(files,proxies,colors,markers,**kwargs):
-	def plots(activations,proxychoices,colors,markers,nmax):
-		i=0
-		for activation in activations:
-			colormarkers=[colors[i]+marker for marker in markers]
-			plotproxies(activation,proxychoices,nmax,colormarkers)
-			i=i+1
-	activations=[util.activations[f] for f in files]
-	plt.figure()
-	plt.yscale('log')
-	if 'ylim' in kwargs:
-		plt.ylim(kwargs.get('ylim'))
-	plots(activations,proxies,colors,markers,8)
-	plotdata(files,colors)
-	savename=' '.join(files)+' _ '+' '.join(proxies)
+	for ac_name,color in ac_name_color.items():
+		n_,norms=bk.getdata(datafolder+'/'+ac_name)
+		plt.plot(n_,norms,color=color,marker='o')
+	
+		norms_table=evalproxies(util.activations[ac_name],proxy_name_style.keys(),n_,WXfolder)	
+		[plt.plot(n_,norms_table[p],color=color,ls=ls) for p,ls in proxy_name_style.items()]
+		
+	savename=' '.join(ac_name_color)
 	plt.savefig('plots/'+savename+'.pdf')
 
-def data_and_proxy_separate(files,proxies,styles='default',subfolder='',same_ylim=False,**kwargs):
-	activations=[util.activations[f] for f in files]
-	n=len(files)
-	if same_ylim:
-		ylim=ylimfromfiles(files)
-	plt.figure(figsize=(10,10))
-	for i in range(n):
+
+"""
+each estimate a different color and style
+"""
+def one_plot_per_activation(ac_names,proxychoices,WXfolder,datafolder,**kwargs):
+
+	for ac_name in ac_names:
 		plt.figure()
 		plt.yscale('log')
-		if same_ylim:
-			plt.ylim(ylim)
-		plotproxies(activations[i],proxies,8,styles=styles)
-		plotdata([files[i]],['b'])
-		savename=files[i]+' _ '+' '.join(proxies)
-		plt.savefig('plots/'+subfolder+savename+'.pdf')
+		if 'ylim' in kwargs:
+			plt.ylim(kwargs.get('ylim'))
 
-def ylimfromfiles(files):
-	mins=[]
-	maxs=[]
-	for filename in files:
-		_,vals=bk.getplotdata(subfolder+filename)
-		mins.append(jnp.min(vals))
-		maxs.append(jnp.max(vals))
-	padratio=1.1#jnp.power(max(maxs)/min(mins),.2)
-	return min(mins),max(maxs)*padratio
+		n_,norms=bk.getdata(datafolder+'/'+ac_name)
+		plt.plot(n_,norms,'bo-')
+
+		norms_table=evalproxies(util.activations[ac_name],proxychoices,n_,WXfolder)
+		[plt.plot(n_,norms_table[p],defaultstyles[p]) for p in proxychoices]
+
+		savename=ac_name+' _ '+' '.join(proxychoices)
+		plt.savefig('plots/singledata/'+savename+'.pdf')
 
 
+def make_colors(l):
+	l=list(l)
+	palette=sns.color_palette(None,len(l))
+	return {l[i]:palette[i] for i in range(len(l))}	
 
-data_only(activationnames,['k','g','b','r','m','c'],'abbrev_separated/')
-data_only(activationnames,['k','g','b','r','m','c'],'abbrev/')
+def cutrange(n_,vals,nmax):
+	n_vals=zip(n_,vals)
+	n_vals=list(filter(lambda nv:nv[1]<=nmax,n_vals))
+	n_,vals=zip(*n_vals)
+	return n_,vals
+
+
+Wtype={'n':'normal','s':'separated'}[sys.argv[1]]
+WXfolder=Wtype
+
+nmax=sys.argv[2]
+datafolder=WXfolder+'/'+str(nmax)
+
+multiple_activations(make_colors(util.activations.keys()),{'polyOCP':'dotted'},WXfolder,datafolder)
+#multiple_activations({'ReLU':'red','HS':'blue'},{'polyOCP':'dotted'},WXfolder,datafolder)
 
 
 ### test all proxies on all functions ###
-data_and_proxy_separate(activationnames,proxynames,subfolder='all/')
+#one_plot_per_activation(activationnames,proxynames,datafolder)
 
-### main proxies ###
-data_and_proxy_separate(activationnames,['OP','polyOP','OCP','polyOCP'],subfolder='mainproxies/')
-
-### polyOCP improvement by direct minimization ###
-data_and_proxy_separate(activationnames,['polyOCP','polyOCP_proxy'],['r','k'],'polyOCP/')
-
-
+#### main proxies ###
+one_plot_per_activation(util.activations.keys(),['OP','polyOP','OCP','polyOCP'],WXfolder,datafolder)
+#
+#### polyOCP improvement by direct minimization ###
+#one_plot_per_activation(activationnames,['polyOCP','polyOCP_proxy'],['r','k'],'polyOCP/')
+#
+#
